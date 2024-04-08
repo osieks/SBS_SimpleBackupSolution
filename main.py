a033ea8972fc34d1b38f15606a15625ffdd77964
Mateusz Dzieżok <oosiekk@gmail.com>
2024-04-08 16:49:11 +0200

    ignoring folders, main before db

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from hashlib import sha256
 
 sha = sha256()
+log = ''
 
 def hash_file(filename):
     with open(filename, 'rb') as f:
@@ -17,9 +18,11 @@ def hash_file(filename):
     return(shaHashed)
 
 def copy_files(src_dir, dest_dir):
+    global log
     # Check if both the source and destination directories exist
     if not os.path.exists(src_dir):
         print(f"Source directory {src_dir} does not exist.")
+        log=log+f"Source directory {src_dir} does not exist.\n"
         return
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir+"/")
@@ -33,13 +36,16 @@ def copy_files(src_dir, dest_dir):
         
         if(os.path.isdir(src_dir+file)):
             print(f"Recursing directory: {file}")
+            log=log+f"Recursing directory: {file}\n"
             if not os.path.exists(dest_dir+file+"/"):
                 os.makedirs(dest_dir+file+"/")
             copy_files(src_dir+file+"/", dest_dir+file+"/")
             
         else:
             print(f"File: {file}")
+            log=log+f"File: {file}\n"
             print(hash_file(src_dir+file))
+            log=log+f"{hash_file(src_dir+file)}\n"
             
             # Construct full file path
             src_file = os.path.join(src_dir, file)
@@ -48,8 +54,13 @@ def copy_files(src_dir, dest_dir):
 
             # Copy the file to the destination directory
             shutil.copy(src_file, dest_file)
+            
+    log=log+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+"\n"
+    with open(destination+"\log_"+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+".txt", "w") as text_file:
+        text_file.write(log)
+    
 
-source = 'C:/Users/mateu/OneDrive/SBS_SimpleBackupSolution/TESTGROUND/'
+source = 'C:/Users/mateu/OneDrive/SBS_SimpleBackupSolution/Source_folder/'
 destination = 'C:/Users/mateu/OneDrive/SBS_SimpleBackupSolution/Backup_folder/'
 
 # Usage
